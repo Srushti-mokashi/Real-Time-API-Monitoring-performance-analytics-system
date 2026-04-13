dbd24ad44acfae0015e4c6a5bdc9a8c0e5daf2c9
Srushti mokashi <srushtimokashi5@gmail.com>
2026-04-13 21:20:03 +0530

    Final: Bind to 0.0.0.0 for Render connectivity

--- task-manager-api/server.js
+++ task-manager-api/server.js
@@ -107,7 +107,7 @@ app.use((req, res) => {
 
 // ---------------- Start Server ----------------
 // Start listening immediately to avoid Render/Heroku boot timeout
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
