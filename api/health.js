796248bc9d414dc27cf7315251bdeb77573f966c
Srushti mokashi <srushtimokashi5@gmail.com>
2026-04-14 19:24:51 +0530

    fix vercel routing

--- api/health.js
+++ api/health.js
@@ -1,7 +1,6 @@
-const db = require("../backend/db");
+import db from "../backend/db.js";
 
 export default async function handler(req, res) {
-
     try {
 
         await db.query("SELECT 1");
@@ -19,5 +18,4 @@ export default async function handler(req, res) {
         });
 
     }
-
 }
